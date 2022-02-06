98a652af8742186651031e52622e011af6aeb7f7
freemeson <freemeson@googlemail.com>
2022-02-06 12:56:27 +0100

    Added windows event resize and drag mouse event to rotate the scene. Time can be defined to be periodic or still by a #define statement

--- renderGlumpy.py
+++ renderGlumpy.py
@@ -11,6 +11,15 @@ vertex = """
 
 
 fragment = """
+//Frozen time
+#define TIME_DEFINITION camDist
+
+//Periodic time
+//#define TIME_DEFINITION mod(time*10.0,camDist)+camDist/2.0
+
+    uniform float phi;
+    uniform float psy;
+    uniform float screen_ratio;
     uniform float time;
     varying vec2 tex_coord0;
 #define MAX_DIST -1e5
@@ -91,42 +100,57 @@ vec4 plane4(in vec4 ro, in vec4 rd, in vec4 origin, in mat4 invLor,in mat4 Einv,
 }
 
 
-void main (void){
-	distanceAndColor dlc=distanceAndColor(vec2(0.0001, 500.0), vec3(0.0, 0.0, 0.0));
+distanceAndColor worldHit(in vec4 ro, in vec4 rd, in vec2 distLim, in float showTime){
+	distanceAndColor dlc=distanceAndColor(distLim, vec3(0.0, 0.0, 0.0));
 	
 	mat4 invBoost = mat4(5.26315789, 0.0, 0.0, 4.73684211,
 	0.0, 1.0, 0.0, 0.0,
 	0.0, 0.0, 1.0, 0.0, 
 	4.73684211, 0.0, 0.0, 4.69904779); //x-direction with 0.9c
-	mat4 noBoost = mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+
+	mat4 invBoost2 = mat4(2.29415734, 0.0, 0.0, -2.0647416, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,-2.0647416, 0.0, 0.0, 2.29415734 );
+	mat4 noBoost = mat4(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0); //this is also the inverse of invBoost 
+	
+	vec4 dc = box4( ro, rd, vec4(0.0, 0.0, 0.0, showTime), invBoost, mat4( 1.0, 0.0, 0.0, 0.0,    0.0, 1.0, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,    0.0, 0.0, 0.0, 1.0 ), vec3(3.0, 3.0, 3.0), dlc.dLim );
+	dlc = opU(dlc, dc.x, dc.yzw);
+
+	dc = box4( ro, rd, vec4(0.0, -8.0, 0.0, showTime), noBoost, mat4( 1.0, 0.0, 0.0, 0.0,    0.0, 1.0, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,    0.0, 0.0, 0.0, 1.0 ), vec3(3.0, 3.0, 3.0), dlc.dLim );
+	dlc = opU(dlc, dc.x, dc.yzw);
+
+	dc = plane4(ro, rd, vec4(0.0, -11.0, 0.0, 0.0), noBoost,mat4( 1.0, 0.0, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,    0.0, 1.0, 0.0, 0.0,    0.0, 0.0, 0.0, 1.0 ), dlc.dLim );	
+	dlc = opU(dlc, dc.x, dc.yzw);
+
+	return dlc;
+}
+
+
+void main (void){
+	distanceAndColor dlc=distanceAndColor(vec2(0.0001, 500.0), vec3(0.0, 0.0, 0.0));
+	
+	
 	float camDist = 50.0;
 	vec4 ro = vec4(0.0, 0.0, camDist, 0.0);
-	vec3 rd3=normalize(vec3(tex_coord0[0]-0.5-ro.x, tex_coord0[1]-0.5-ro.y,camDist+1.5-ro.z)); //z is funny, I know
+	vec3 rd3=normalize(vec3(tex_coord0[0]-0.5-ro.x, screen_ratio*(tex_coord0[1]-0.5-ro.y),camDist+1.5-ro.z)); //z is funny, I know
    vec4 rd = vec4(rd3,-1.0);
 	
-	float phi = time/4.0;
+	//float phi = time/4.0;
    mat4 rotationXZ = mat4( cos(phi),0.0, -sin(phi), 0.0, 0.0, 1.0, 0.0, 0.0, sin(phi), 0.0, cos(phi), 0.0, 0.0, 0.0, 0.0, 1.0  );
 	
-	float psy = 0.3;
+	//float psy = 0.3;
 	
 	mat4 rotationYZ = mat4( 1.0, 0.0, 0.0, 0.0, 0.0, cos(psy), -sin(psy), 0.0, 0.0, sin(psy), cos(psy), 0.0, 0.0, 0.0, 0.0, 1.0 );
 
 	ro = rotationXZ * rotationYZ* ro;
 	rd = rotationXZ * rotationYZ*rd;
-	float showTime= camDist;//mod(time*10.0,camDist)+camDist/2.0;
-	vec4 dc = box4( ro, rd, vec4(0.0, 0.0, 0.0, showTime), invBoost, mat4( 1.0, 0.0, 0.0, 0.0,    0.0, 1.0, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,    0.0, 0.0, 0.0, 1.0 ), vec3(3.0, 3.0, 3.0), dlc.dLim );
-	dlc = opU(dlc, dc.x, dc.yzw);
+	float showTime= TIME_DEFINITION;
 
-	dc = box4( ro, rd, vec4(0.0, -8.0, 0.0, 0.0), noBoost, mat4( 1.0, 0.0, 0.0, 0.0,    0.0, 1.0, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,    0.0, 0.0, 0.0, 1.0 ), vec3(3.0, 3.0, 3.0), dlc.dLim );
-	dlc = opU(dlc, dc.x, dc.yzw);
-
-	dc = plane4(ro, rd, vec4(0.0, -11.0, 0.0, 0.0), noBoost,mat4( 1.0, 0.0, 0.0, 0.0,    0.0, 0.0, 1.0, 0.0,    0.0, 1.0, 0.0, 0.0,    0.0, 0.0, 0.0, 1.0 ), dlc.dLim );	
-	dlc = opU(dlc, dc.x, dc.yzw);
+	
+	dlc = worldHit(ro, rd, dlc.dLim,showTime);
+	
 	//gl_FragColor = vec4(dc.yzw,1.0);
 	//gl_FragColor = vec4(0.5,0.5,0.5,1.0);
    gl_FragColor = vec4(dlc.color*exp(-0.016*(dlc.dLim.y-camDist)),1.0);//*frag_color;
 }
-
 // void main() { gl_FragColor = vec4(tex_coord0.y, 0.5*sin(time)+0.5, 0.0, 1.00); } 
 """
 
@@ -144,6 +168,9 @@ quad['position'] = (-1,-1),   (-1,+1),   (+1,-1),   (+1,+1)
 texture_coords = np.array([[0, 1], [0, 0], [1, 1], [1, 0]])
 
 quad['vTexCoords0'] = texture_coords
+quad['phi'] = 0.0
+quad['psy'] = 0.3
+quad['screen_ratio'] = 1.0
 
 # Tell glumpy what needs to be done at each redraw
 @window.event
@@ -152,6 +179,21 @@ def on_draw(dt):
     quad['time']=app.clock.time.time()-t0
    # print(app.clock.time.time())
     quad.draw(gl.GL_TRIANGLE_STRIP)
+
+
+@window.event
+def on_resize(width, height):
+    quad['screen_ratio'] = height/width;
+
+
+@window.event
+def on_mouse_drag(x,y,dx,dy,buttons):
+   # print('drag ', x, ' ',y, ' ', dx, ' ',dy )
+    #print(window.width)
+    quad['phi'] += 3*dx/window.width
+    quad['psy'] += 3*dy/window.height
+    quad['psy'] = max(-0.1, min(0.9, quad['psy']))
+    
 
 # Run the app
 app.run()
